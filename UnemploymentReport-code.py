2cd6bd1d9b83548fb2200a412c18fd84e44e9107
dilang02 <105160866+dilang02@users.noreply.github.com>
2022-10-27 07:59:09 -0400

    Update UnemploymentReport-code.py

--- UnemploymentReport-code.py
+++ UnemploymentReport-code.py
@@ -33,27 +33,3 @@ dates = [d["date"] for d in data]
 rates = [float(d["value"]) for d in data]
 fig = line(x=dates,y=rates,title="United States Unemployment Rate over Time",labels={"x":"Month","y":"Unemployment Rate"})
 fig.show()
-
-# CSV A-C
-
-from pandas import read_csv
-request_url = f"https://www.alphavantage.co/query?function=UNEMPLOYMENT&apikey={API_KEY}&datatype=csv"
-df = read_csv(request_url)
-print(df.head())
-print(df.columns)
-print(len(df))
-
-print("----------------------")
-print("LATEST UNEMPLOYMENT RATE:")
-first_row = df.iloc[0]
-print(f"{first_row['value']}%","as of",first_row["timestamp"])
-
-this_year_df = df[df["timestamp"].str.contains("2022-")]
-print(this_year_df)
-print("-------------------")
-print("AVG. UNEMPLOYMENT THIS YEAR:",f"{this_year_df['value'].mean()}%")
-print("NO MONTHS:",len(this_year_df))
-
-from plotly.express import line
-fig = line(x=df["timestamp"],y=df["value"],title="United States Unemployment Rate over time",labels={"x":"Month","y":"Unemployment Rate"})
-fig.show()
